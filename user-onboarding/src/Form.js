5ddd03407840dfad8aea0128376ce3dd7a0d5fde
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-09-19 13:32:56 +0100

    Formik form created

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-const Form = () =>{
+const UserForm = () =>{
     return(
-        <>
-        </>
+        <Formik
+        // validationSchema={validationSchema}
+        // validate={validate}
+        // initialValues={initialFriendForm}
+        // onSubmit={onSubmit}
+        render={props => {
+          return (
+            // we will use pre-baked components
+            // supplied by formik lib (like Formik)
+            <Form>
+              {/* {
+                !props.dirty && <div>time to start typing!!</div>
+              } */}
+              <div>
+                <label>
+                  Name
+                  <Field name='name' type='text' placeholder='Name' />
+                  <ErrorMessage name='name' component='div' />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Email
+                  <Field name='email' type='email' placeholder='Email' />
+                  <ErrorMessage name='email' component='div' />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Password
+                  <Field name='password' type='password' placeholder='Password' />
+                  <ErrorMessage name='password' component='div' />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Terms of Service
+                  <Field name='ToS' type='checkbox' />
+                  <ErrorMessage name='ToS' component='div' />
+                </label>
+              </div>
+              <button type='submit'>Submit</button>
+            </Form>
+          );
+        }}
+      />
     )
 }
-export default  Form
+export default  UserForm
